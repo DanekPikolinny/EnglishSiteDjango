c29943ea7c48d4c81a8867fbd11d330ef07c1f85
Danek <danek.lagutkov@yandex.ru>
2024-02-22 00:00:57 +0300

    Renamed Project Name

--- english_tenses/english_tenses/urls.py
+++ english_tenses/english_tenses/urls.py
@@ -1,4 +1,4 @@
-"""animal_blog URL Configuration
+"""english_tenses URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
