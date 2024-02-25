f7f2c2402be4896303f1f0400a57233a74b58c25
Danek <danek.lagutkov@yandex.ru>
2024-02-25 23:25:35 +0300

    Updated urls, views and added some t-plates

--- english_tenses/vocabulary_app/urls.py
+++ english_tenses/vocabulary_app/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', get_welcome_page),
-    path('text/<int:text_number>/', get_text_page),
+    path('text/', get_main_text_page),
+    path('text/<int:text_number>/', get_text_page, name='choosen_text'),
 ]
--- english_tenses/vocabulary_app/views.py
+++ english_tenses/vocabulary_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.urls import reverse
 
 
 # Create your views here.
@@ -6,5 +7,34 @@ def get_welcome_page(request):
     return render(request, 'vocabulary_app/index.html')
 
 
+def get_main_text_page(request):
+    redirected_urls_dict = {
+        'url1': reverse('choosen_text', args=[1]),
+        'url2': reverse('choosen_text', args=[2]),
+        'url3': reverse('choosen_text', args=[3]),
+    }
+    return render(request, 'vocabulary_app/main_text_page.html', context=redirected_urls_dict)
+
+
+texts_data = {
+    1: {
+        'title': "Bayeux",
+        'text': 1
+    },
+    2: {
+        'title': "Ilon Mask",
+        'text': 2
+    },
+    3: {
+        'title': "Johny Depp",
+        'text': 3
+    }
+}
+
+
 def get_text_page(request, text_number):
-    return HttpResponse(f"<h1>{text_number} - is your number of choosen text</h1>")
+    data = {
+        'title': texts_data[text_number]['title'],
+        'text': texts_data[text_number]['text']
+    }
+    return render(request, 'vocabulary_app/text_page.html', context=data)
