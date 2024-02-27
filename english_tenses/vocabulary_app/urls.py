60a3312709f2ead8732be1341ea13de347605064
Danek <danek.lagutkov@yandex.ru>
2024-02-27 21:02:48 +0300

    Updated Templates, Views, Urls in vocabulary_app, added actvie references

--- english_tenses/vocabulary_app/urls.py
+++ english_tenses/vocabulary_app/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', get_welcome_page),
     path('text/', get_main_text_page),
-    path('text/<int:text_number>/', get_text_page, name='choosen_text'),
+    path('text/<int:text_number>/', get_text_page, name='choosen_text_by_num'),
+    path('text/<str:text_title>/', get_text_page_by_title, name='choosen_text_by_title'),
 ]
--- english_tenses/vocabulary_app/views.py
+++ english_tenses/vocabulary_app/views.py
@@ -3,33 +3,23 @@ from django.urls import reverse
 
 
 # Create your views here.
+
+texts_data = {
+    'titles': ["bayeux", "ilon_Mask", "johny_Depp"],
+    'texts': []
+}
+
+
 def get_welcome_page(request):
     return render(request, 'vocabulary_app/index.html')
 
 
 def get_main_text_page(request):
-    redirected_urls_dict = {
-        'url1': reverse('choosen_text', args=[1]),
-        'url2': reverse('choosen_text', args=[2]),
-        'url3': reverse('choosen_text', args=[3]),
+    data = {
+    'titles': ["bayeux", "ilon_Mask", "johny_Depp"],
+    'texts': []
     }
-    return render(request, 'vocabulary_app/main_text_page.html', context=redirected_urls_dict)
-
-
-texts_data = {
-    1: {
-        'title': "Bayeux",
-        'text': 1
-    },
-    2: {
-        'title': "Ilon Mask",
-        'text': 2
-    },
-    3: {
-        'title': "Johny Depp",
-        'text': 3
-    }
-}
+    return render(request, 'vocabulary_app/main_text_page.html', context=data)
 
 
 def get_text_page(request, text_number):
@@ -38,3 +28,10 @@ def get_text_page(request, text_number):
         'text': texts_data[text_number]['text']
     }
     return render(request, 'vocabulary_app/text_page.html', context=data)
+
+
+def get_text_page_by_title(request, text_title):
+    title = {
+        'title': text_title,
+    }
+    return render(request, 'vocabulary_app/text_page.html', context=title)
