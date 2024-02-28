4b6fb6c177dde911624451a28354a79cd5f81366
Danek <danek.lagutkov@yandex.ru>
2024-02-28 22:13:24 +0300

    Added index.css which changes main 'vocabulary' page & added model 'TextSection'

--- english_tenses/vocabulary_app/models.py
+++ english_tenses/vocabulary_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class TextSection(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rating = models.IntegerField()
--- english_tenses/vocabulary_app/urls.py
+++ english_tenses/vocabulary_app/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', get_welcome_page),
-    path('text/', get_main_text_page),
+    path('text/', get_main_text_page, name='vocabulary_main_text_page'),
     path('text/<int:text_number>/', get_text_page, name='choosen_text_by_num'),
     path('text/<str:text_title>/', get_text_page_by_title, name='choosen_text_by_title'),
 ]
