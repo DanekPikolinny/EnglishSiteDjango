5ada7d839a3fb76efd630be6f63bef93b06106f5
Danek <danek.lagutkov@yandex.ru>
2024-02-29 22:54:09 +0300

    Added some objects to vocabulary model "TextSection"

--- english_tenses/vocabulary_app/models.py
+++ english_tenses/vocabulary_app/models.py
@@ -5,3 +5,6 @@ class TextSection(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     rating = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.title} with rated: {self.rating}'
