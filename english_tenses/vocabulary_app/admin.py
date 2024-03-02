3648ec8392afeac50ba4eda872d950b3cfef8696
Danek <danek.lagutkov@yandex.ru>
2024-03-02 23:49:01 +0300

    Modified admin panel

--- english_tenses/vocabulary_app/admin.py
+++ english_tenses/vocabulary_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TextSection
 
 # Register your models here.
+@admin.register(TextSection)
+class TextSectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'text', 'rating']
+    list_editable = ['text', 'rating']
